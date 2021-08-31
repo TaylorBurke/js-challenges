753311042cb06a340a7948ba399df9d45d9349d7
Taylor Burke <twillyb@gmail.com>
2021-08-30 18:20:36 -0600

    begin challenge

---
+++ pages.js
@@ -0,0 +1,8 @@
+// Write a function that returns the closest chapter to the current page you are at. If two chapters are similarly close,
+// return whichever has the higher page.
+
+let book = {
+  "Chapter 1": 1,
+  "Chapter 2": 15,
+  "Chapter 3": 37,
+};
