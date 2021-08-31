51292c2e617a6205f590d76a8fa2ef63f88df0d4
Taylor Burke <twillyb@gmail.com>
2021-08-31 15:13:42 -0600

    chapters fin

--- pages.js
+++ pages.js
@@ -6,3 +6,18 @@ let book = {
   "Chapter 2": 15,
   "Chapter 3": 37,
 };
+
+const closestToPage = (book, page) => {
+  let output;
+  let closestMatch = Math.max(...Object.values(book));
+  for (chapter in book) {
+    if (Math.abs(page - book[chapter]) <= closestMatch) {
+      closestMatch = Math.abs(page - book[chapter]);
+      output = chapter;
+    }
+  }
+  return output;
+};
+
+console.log(closestToPage(book, 8));
+//should return "Chapter 2"
