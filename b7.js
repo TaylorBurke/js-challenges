ed826c655267ca1b0c1c5a16a17b51cbb38d3585
Taylor Burke <twillyb@gmail.com>
2021-09-05 15:20:03 -0600

    burg 7

---
+++ b7.js
@@ -0,0 +1,6 @@
+// Your spouse wants a copy of the stolen items.
+// Given an object containing the stolen items, return a copy of that list.
+
+// https://edabit.com/challenge/cEbhy9eWeNudEuxut
+
+const makeCopy = (obj) => ({ ...obj });
